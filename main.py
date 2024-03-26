3f910b24925ab5202bbcec2c4f40669785d68227
Ajay <iamajay6122@gmail.com>
2024-03-26 09:26:38 +0000

    Reconfigured Main.py

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-import moviepy.editor as mpy
-from moviepy.video.tools.segmenting import findObjects
-from modules.text_gen import text_generation
 from modules.video_gen import generate_video
-from modules.audio_gen import generate_audio
-
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-White = (255, 255, 255)
-ScreenSize = (720, 460)
-VerticalMargin = 30
-HorizontalMargin = 100
-
 def main():
-    text = text_generation("Write a Aesthetic General Knowledge")
-    generate_audio(text=text)
-    generate_video(text=text, water_mark="Watermark")
+    text = "Thriller Horror Story"
+    generate_video(text, 2)
 
 if __name__ == "__main__":
     main()
--- modules/video_gen.py
+++ modules/video_gen.py
@@ -1,31 +1,64 @@
 from moviepy.video.tools.subtitles import SubtitlesClip
-from moviepy.editor import *
+from moviepy.editor import CompositeAudioClip, concatenate_audioclips
+from moviepy.editor import VideoFileClip, concatenate_videoclips, TextClip, ImageClip, CompositeVideoClip, AudioFileClip
+import json, os
+import whisper
+from modules.text_gen import generate_script, eleven_speak
 
-def generate_video(speech: list):
-    with open('samp.txt', 'r') as file:
-        text = file.read()
+def speech_to_text(audio_file):
+    model = whisper.load_model("base")
+    data = model.transcribe(audio_file, word_timestamps=True)
+
+    start = [data['segments'][i]['words'][j]['start'] for i in range(len(data['segments'])) for j in range(len(data['segments'][i]['words']))]
+    end = [data['segments'][i]['words'][j]['end'] for i in range(len(data['segments'])) for j in range(len(data['segments'][i]['words']))]
+    text = [data['segments'][i]['words'][j]['word'] for i in range(len(data['segments'])) for j in range(len(data['segments'][i]['words']))]
+
+    return [start, end, text]
+
+def generate_video(video_title: str, number_of_videos: int):
+    generate_script(video_title, number_of_videos)
     
-    lst = text.split(' ')
+    for i in range(number_of_videos):
+        print(i)
 
-    clips = []
+        story_path = "story"
+        with open(f'{story_path}/story_{i}.json', 'r') as f:
+            text = json.load(f)
+            text = text['story']
+            text = [word.replace('\n', '') for word in text]
+            new_text = ''.join(text)
+            print(new_text)
+            eleven_speak(new_text, f'story_{i}')
 
-    generator = lambda txt: TextClip(txt, 
-                                fontsize=70, 
-                                color='white', 
-                                bg_color='none', 
-                                font='Arial-Bold',
-                                method = 'caption',
-                                size = (1920, 1080))
-
-    clips = [CompositeVideoClip([ImageClip('Z_1.jpg').set_duration(0.5).set_position('center')], size=(1920, 1080)) for _ in lst]
-
-    subs = [((speech[0][k], speech[1][k]), speech[2][k]) for k in range(len(speech[0]))]
+        with open(f'{story_path}/story_{i}.json', 'r') as file:
+            print(f'Generating video for story {i}')
+            text = json.load(file)
+            text = text['story']
         
+        words = text.split(' ')
 
-    subtitles = SubtitlesClip(subs, generator)
+        generator = lambda txt: TextClip(txt, 
+                                    fontsize=70, 
+                                    color='white', 
+                                    bg_color='none', 
+                                    font='Arial-Bold',
+                                    method = 'caption',
+                                    size = (1920, 1080))
 
-    final = concatenate_videoclips(clips)
-    audio = AudioFileClip("samp.mp3")
-    final = final.set_audio(audio)
-    final_complete = CompositeVideoClip([final, subtitles.set_position(('center', 'bottom'))])
-    final_complete.write_videofile("output0.mp4", fps=24, codec = 'libx264')
+        # def clip_generator():
+        #     for _ in words:
+        #         yield CompositeVideoClip([ImageClip('Z_1.jpg').set_duration(0.5).set_position('center')], size=(720, 1280))
+
+        # clips = concatenate_videoclips(clip_generator(), method="compose")
+
+        speech = speech_to_text(f'audio/story_{i}.mp3')
+        print(f'Accessing speech for story {i}')
+        subs = [((speech[0][k], speech[1][k]), speech[2][k]) for k in range(len(speech[0]))]
+
+        subtitles = SubtitlesClip(subs, generator)
+        audio_clip = AudioFileClip(f'audio/story_{i}.mp3')
+        video = VideoFileClip('./assets/BG.mp4')
+        final_complete = CompositeVideoClip([video.set_duration(audio_clip.duration), subtitles.set_position(('center', 'center'))])
+        final_complete = final_complete.set_audio(audio_clip)
+        final_complete.write_videofile(f"output_{i}.mp4" , fps=24, bitrate = '8000k', preset = 'fast', codec = 'libx264')
+        final_complete.close()
