a933b4e21b6f28b4adce179b47e3a97f07385450
ShadowConYT <iamajay6122@gmail.com>
2024-04-14 12:37:30 +0530

    Refactor code to remove unused modules and files

--- main.py
+++
@@ -1,7 +0,0 @@
-from modules.video_gen import generate_video
-def main():
-    text = "Thriller Horror Story"
-    generate_video(text, 2)
-
-if __name__ == "__main__":
-    main()
--- modules/audio_gen.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3 as tts # will change it to something cooler
-
-def generate_audio(text: str):
-    engine = tts.init()
-    engine.save_to_file(text, 'sample.mp3')
-    engine.runAndWait()
